b39c52ba2f215e5efb6936e2eeddf54e7aaac321
Atreya Bain <Atreya Bain>
2016-05-06 19:10:21 +0530

    Included  "I am Spartacus" and improved message detection.

--- get_message.py
+++ get_message.py
@@ -30,9 +30,12 @@ def on_state_update(state_update):
 
 def processMsg(msg, cid):
     
-    if msg.startswith("Tose, lolme") or msg.startswith("tose, lolme"):
-        randomMessages = ["I don't hold grudges, my father did and I always hated him for it", " Say what you want about deaf people..."," My wife and I were happy for twenty years; then we met.", "My grandfather has the heart of a lion and a lifetime ban from the local zoo.","When you throw a boomerang and it doesnt return, you lost a stick","I refused to believe my roadworker father was stealing from his job, but when I got home, all the signs were there."]
-        os.system("python send_message.py \""+ random.choice(randomMessages) +"\" "+cid)
+    if msg.startswith("Tose") or msg.startswith("tose") :
+        if "lol me" in msg:
+            randomMessages = ["I don't hold grudges, my father did and I always hated him for it", " Say what you want about deaf people..."," My wife and I were happy for twenty years; then we met.", "My grandfather has the heart of a lion and a lifetime ban from the local zoo.","When you throw a boomerang and it doesnt return, you lost a stick","I refused to believe my roadworker father was stealing from his job, but when I got home, all the signs were there."]
+            os.system("python send_message.py \""+ random.choice(randomMessages) +"\" "+cid)
+        if "I am Spartacus" in msg or "i am spartacus" in msg or "I am spartacus" in msg
+            os.system("python send_message.py \""+"No, I am Spartacus") +"\" "+cid)
 
 
 if __name__ == '__main__':
