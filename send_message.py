455fcb61db12d77455385333bbfccd832f76b9b5
Christopher Rose <atreyabain@outlook.com>
2016-05-06 08:20:24 +0000

    Removed the need to change the conversation ID in the code.

--- send_message.py
+++ send_message.py
@@ -4,16 +4,25 @@ import hangups
 import sys
 
 # ID of the conversation to send the message to.
-CONVERSATION_ID = 'UgzTQ7JmCpWG_Peinjx4AaABAagB_IuSBQ'
-
+# CONVERSATION_ID = 'UgzTQ7JmCpWG_Peinjx4AaABAagB_IuSBQ'
+#Handling the message
+if len(sys.argv)==1: 
+    MESSAGE = "Yolo"        #No arguments - Why, don't ask us
+else: 
+    MESSAGE = sys.argv[1]
+    
+#Handling the Conversation ID
+if len(sys.argv)>=3:
+    CONVERSATION_ID = sys.argv[2]        #Set input Conversation id
+else
+    CONVERSATION_ID = 'UgyT6DYhh50bUOijMVh4AaABAQ'        #Post to the Tose Group
 ## Some conversation IDs for testing.
 # Rithvik Vibhu: UgzTQ7JmCpWG_Peinjx4AaABAagB_IuSBQ
 # ToSe group:  UgyT6DYhh50bUOijMVh4AaABAQ
 # Test group: Ugw-YMKhMfDDCpS7KiV4AaABAQ
 
-# Plain-text content of the message to send.
-MESSAGE = sys.argv[1]
-print("Sent hangouts message: ", MESSAGE)
+#Below code removed as this confirmation is not really required as the header is returned
+#print("Sent hangouts message: ", MESSAGE)
 
 # Path where OAuth refresh token is saved, allowing hangups to remember yourcredentials.
 REFRESH_TOKEN_PATH = 'refresh_token.txt'
