74f3b5fa70bd0ad0a4dc20df7c0c61e89102f78b
Rithvik Vibhu <rithvikvibhu@gmail.com>
2016-05-06 12:41:00 +0000

    Tose, lolme

--- get_message.py
+++ get_message.py
@@ -1,12 +1,12 @@
 #!python3
 import asyncio
 import hangups
-import sys
+import sys, os
+import random
 from google.protobuf import descriptor_pb2
 
 REFRESH_TOKEN_PATH = 'refresh_token.txt'
-CONVERSATION_ID = 'DEFAULT: UgzTQ7JmCpWG_Peinjx4AaABAagB_IuSBQ'
-MESSAGE = 'Test Message'
+# CONVERSATION_ID = 'DEFAULT: UgzTQ7JmCpWG_Peinjx4AaABAagB_IuSBQ'
 
 def main():
     cookies = hangups.auth.get_auth_stdin(REFRESH_TOKEN_PATH)
@@ -19,31 +19,20 @@ def main():
 def on_state_update(state_update):
     
     if state_update.HasField('conversation'):
-        print(state_update.conversation)
+        # print(state_update.conversation)
         CONVERSATION_ID = state_update.conversation.conversation_id.id
         msg = state_update.event_notification.event.chat_message.message_content.segment[0].text
         print("ConversationId: ",CONVERSATION_ID)
         print("msg: ",msg)
-    
-    # request = hangups.hangouts_pb2.SendChatMessageRequest(
-    #     request_header=client.get_request_header(),
-    #     event_request_header=hangups.hangouts_pb2.EventRequestHeader(
-    #         conversation_id=hangups.hangouts_pb2.ConversationId(
-    #             id=CONVERSATION_ID
-    #         ),
-    #         client_generated_id=client.get_client_generated_id(),
-    #     ),
-    #     message_content=hangups.hangouts_pb2.MessageContent(
-    #         segment=[hangups.ChatMessageSegment(MESSAGE).serialize()],
-    #     ),
-    # )
+        processMsg(msg, CONVERSATION_ID)
 
-    # try:
-    #     # Make the request to the Hangouts API.
-    #     yield from client.send_chat_message(request)
-    # finally:
-    #     # Disconnect the hangups Client to make client.connect return.
-    #     yield from client.disconnect()
+
+
+def processMsg(msg, cid):
+    
+    if msg.startswith("Tose, lolme") or msg.startswith("tose, lolme"):
+        randomMessages = ["I don't hold grudges, my father did and I always hated him for it", " Say what you want about deaf people..."," My wife and I were happy for twenty years; then we met.", "My grandfather has the heart of a lion and a lifetime ban from the local zoo.","When you throw a boomerang and it doesnt return, you lost a stick","I refused to believe my roadworker father was stealing from his job, but when I got home, all the signs were there."]
+        os.system("python send_message.py \""+ random.choice(randomMessages) +"\" "+cid)
 
 
 if __name__ == '__main__':
