73b16417a9e4bb92e47f1f032783c8c6a3084354
Atreya Bain <Atreya Bain>
2016-05-08 22:23:57 +0530

    Commit storing and updating of files

--- fix-py3.sh
+++ fix-py3.sh
@@ -1,12 +1,13 @@
 
 COMMITID1 = `git log -1 | grep commit`
-COMMITID2 = `git log -1 | grep commit`
+COMMITID2 = `cat conv_commit`
 if[ COMMITID1 != COMMITID2 ]
 then
     cp get_message.py get_message-py3.py
     sed -i -e 's/\"python /\"python3 /g' get_message-py3
     cp script.py script-py3.py
     sed -i -e 's/\"python /\"python3 /g' script-py3
+    git log -1 | grep commit > conv_commit
     echo "Converted and/or updated"
 else
     echo "Already Updated"
