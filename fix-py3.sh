b1fc16b9d72b33d3bebfc13d64ff45663e8f387a
BuildTools <unconfigured@null.spigotmc.org>
2016-05-08 23:11:42 +0530

    Fixed conversion scripts (Learnt about bash too) and updating should be better

--- fix-py3.sh
+++ fix-py3.sh
@@ -1,14 +1,14 @@
-
-COMMITID1 = `git log -1 | grep commit`
-COMMITID2 = `cat conv_commit`
-if[ COMMITID1 != COMMITID2 ]
-then
-    cp get_message.py get_message-py3.py
-    sed -i -e 's/\"python /\"python3 /g' get_message-py3
-    cp script.py script-py3.py
-    sed -i -e 's/\"python /\"python3 /g' script-py3
-    git log -1 | grep commit > conv_commit
-    echo "Converted and/or updated"
+#!/bin/bash
+COMMITIA=`git log -1 | grep "commit"`
+COMMITIB=`cat conv_commit`
+if [ "$COMMITIA" = "$COMMITIB" ]; then
+   echo "Updated. Use the -py3 appended files"
 else
-    echo "Already Updated"
+   cp get_message.py get_message-py3.py
+   sed -i -e 's/\"python /\"python3 /g' get_message-py3
+   cp script.py script-py3.py
+   sed -i -e 's/\"python /\"python3 /g' script-py3
+   git log -1 | grep commit > conv_commit
+   echo "Converted and/or updated"
 fi
+
