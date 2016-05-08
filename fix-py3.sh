23ec826cec8dcf47a6ccaae8d7b69c8c41a60781
BuildTools <unconfigured@null.spigotmc.org>
2016-05-08 23:45:04 +0530

    Changed conversion stuff.

--- fix-py3.sh
+++ fix-py3.sh
@@ -4,10 +4,10 @@ COMMITIB=`cat conv_commit`
 if [ "$COMMITIA" = "$COMMITIB" ]; then
    echo "Updated. Use the -py3 appended files"
 else
-   cp get_message.py get_message-py3.py
-   sed -i -e 's/\"python /\"python3 /g' get_message-py3
-   cp script.py script-py3.py
-   sed -i -e 's/\"python /\"python3 /g' script-py3
+   cp -v get_message.py get_message-py3.py
+   sed -i -e 's/\"python /\"python3 /g' get_message-py3.py
+   cp -v script.py script-py3.py
+   sed -i -e 's/\"python /\"python3 /g' script-py3.py
    git log -1 | grep commit > conv_commit
    echo "Converted and/or updated"
 fi
