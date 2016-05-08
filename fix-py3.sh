fbe9810882f41c0ca75504cd136f27a6966d2191
ATMS-s1 <atreyabain@outlook.com>
2016-05-09 00:11:32 +0530

    Changed command output to give more information

--- fix-py3.sh
+++ fix-py3.sh
@@ -2,7 +2,7 @@
 COMMITIA=`git log -1 | grep "commit"`
 COMMITIB=`cat conv_commit`
 if [ "$COMMITIA" = "$COMMITIB" ]; then
-   echo "Updated. Use the -py3 appended files"
+   echo "Already updated. Use the -py3 appended files. Remove the conv_commit file to force update the py3 files."
 else
    cp -v get_message.py get_message-py3.py
    sed -i -e 's/\"python /\"python3 /g' get_message-py3.py
