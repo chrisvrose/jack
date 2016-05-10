cdbd847ea74a504255dbcbbd553e831ce03e12ab
ATMS-s1 <atreyabain@outlook.com>
2016-05-10 17:55:45 +0530

    Adding permarun stuff

---
+++ get_messages-py3.sh
@@ -0,0 +1,6 @@
+while true
+do
+python3 get_message-py3.py
+echo "Restarting in 15 seconds. Press CTL+C to cancel"
+sleep 5
+done
---
+++ get_messages.sh
@@ -0,0 +1,6 @@
+while true
+do
+python get_message.py
+echo "Restarting in 15 seconds. Press CTL+C to cancel"
+sleep 5
+done
