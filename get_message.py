83a6015a70b6b7adc9f1063dc7e3cbce17fc2c78
Christopher Rose <christophervrose@outlook.com>
2016-08-21 13:04:17 +0530

    Added ability to call by name, added custom replies to custom question that can be added to the JsON file.

--- get_message.py
+++ get_message.py
@@ -4,13 +4,22 @@ import hangups
 import sys, os
 import random
 import time
+import json
+#from pprint import pprint
 from google.protobuf import descriptor_pb2
 from cleverbot import Cleverbot
 
+# Calls up the cleverbot instance
 cb = Cleverbot()
 
 
-REFRESH_TOKEN_PATH = 'refresh_token.txt'    #Stores the refresh token after using a auth token once
+REFRESH_TOKEN_PATH = 'refresh_token.txt'    # Stores the refresh token after using a auth token once
+
+with open('prop.json') as data_file:
+   data = json.load(data_file)
+
+#print(data)
+#print(data["name"])
 
 def main():
     global client
@@ -22,33 +31,47 @@ def main():
 
 @asyncio.coroutine
 def on_state_update(state_update):
-    if state_update.HasField('conversation'):
-        # print(state_update.conversation)
+    if (state_update.HasField('conversation')):
+        #if (state_update.HasField('conversation') and (state_update.conversation.type != 'CONVERSATION_TYPE_ONE_TO_ONE')):
+        #print(state_update.conversation)
         CONVERSATION_ID = state_update.conversation.conversation_id.id
-        msg = state_update.event_notification.event.chat_message.message_content.segment[0].text
-        print("Message captured: ",msg)
+        segment_length = range(len(state_update.event_notification.event.chat_message.message_content.segment))
+        #msg = state_update.event_notification.event.chat_message.message_content.segment[0].text
+        # Join all message segments into one piece properly, and not have one message segment
+        msg = " ".join([state_update.event_notification.event.chat_message.message_content.segment[x].text for x in segment_length])
+        pmsg = msg.lower()
         if(state_update.event_notification.event.self_event_state.user_id.chat_id != state_update.event_notification.event.sender_id.chat_id):
-            if msg.lower().startswith("@bot"):
-                if "identify" in msg.lower():
-                    processMsg("Its me, Jack!",CONVERSATION_ID)
-                if "exit" in msg.lower():
+            print("Message captured: ",msg)
+            #print(state_update.event_notification.event.chat_message)
+            if pmsg.startswith((data["name"].lower()+",")):
+                tmsg = pmsg[(len(data['name'])+1):].strip()
+                print(tmsg)
+                if "identify" in pmsg:
+                    processMsg(("Its me, "+data["name"]+"!"),CONVERSATION_ID)
+                if "exit" in pmsg:
                     sys.exit(0)
-                if "cid" in msg.lower():
+                if "cid" in pmsg:
                     processMsg(CONVERSATION_ID,CONVERSATION_ID)
-                if "time" in msg.lower():
+                if "time" in pmsg:
                     stime = time.strftime('%l:%M%p, %b %d %Y, %z')
                     processMsg(stime,CONVERSATION_ID)
+                if tmsg in data:
+                    processMsg(random.choice(data[tmsg]),CONVERSATION_ID)
             else:
-                processMsg(msg, CONVERSATION_ID, True)
+                processMsg(msg, CONVERSATION_ID, 1)
 
-def processMsg(msg, cid,rep = False):
-    #This is implemented like such - passing true to the function uses the cleverbot function, else the message is sent as such
-    if(rep):
+
+
+def processMsg(msg, cid,rep = 0):
+    # This is implemented like such - passing true to the function uses the cleverbot function, else the message is sent as such
+    if(rep==1):
         reply = cb.ask(msg)
         print("[processMsg]", reply)
         asyncio.async(send_message(client,reply,cid))
     else:
         asyncio.async(send_message(client,msg,cid))
+
+
 
 
 def send_message(client,msg,cid):
