a8db1154e4ff6caa1fa5385174d2dc2f6508fb47
Atreya Bain <atreyabain@gmail.com>
2017-02-10 11:40:56 +0530

    Add new stuff, change questions and add general search

--- et.py
+++ et.py
@@ -13,10 +13,27 @@ def search(query,ep):
 	eps = html.escape(query.replace("(ep)",epsf(ep)))
 	print(eps)
 	rss = feedparser.parse("https://extratorrent.cc/rss.xml?type=search&search="+eps)
-	link = rss["entries"][0]["links"][0]["href"].replace("http://","https://")
-	mgn = ttom(link)
-	print("Found:",mgn)
-	return(mgn)
+	#link = rss["entries"][0]["links"][0]["href"].replace("http://","https://")
+	mgn = rss
+	#mgn = ttom(link)
+	#print("Found:",mgn)
+	# No entries found
+	if not mgn["entries"]:
+		return("Not found. Sorry")
+	else:
+		return(mgn["entries"][0]["magneturi"])
+
+def search_gen(query,number):
+	rep = {}
+	rss = feedparser.parse("https://extratorrent.cc/rss.xml?type=search&search="+query)
+	number = number if len(rss["entries"]) > number else len(rss["entries"])
+	for i in range(number):
+		if(rss["entries"][i]["tags"][0]["term"].startswith("Adult")):
+			rep[i] = {"NSFW. Ignoring.":""}
+		else:
+			rep[i] = {rss["entries"][i]["title"]:rss["entries"][i]["magneturi"]}
+	return(rep)
+	#return str
 
 # Formatting numbers:
 def epsf(ep):
@@ -26,16 +43,16 @@ def epsf(ep):
 	rep = "S" + season + "E" + episode
 	return(rep)
 
-def ttom(link):
-	details = urlopen(Request(link, headers={'User-Agent': 'Mozilla'})).read().decode()
-	if not details:
-		return ''
-	#match = re.search(r'href="(magnet:\?*)"', details)
-	match = re.search(r'(href=\"magnet:.*nce\") title=\"Magnet link\"', details)
-	if not match:
-		return ''
-	# Removing href and return
-	return match.group(1).replace("href=\"","").replace("\"","")
+#def ttom(link):
+#	details = urlopen(Request(link, headers={'User-Agent': 'Mozilla'})).read().decode()
+#	if not details:
+#		return ''
+#	#match = re.search(r'href="(magnet:\?*)"', details)
+#	match = re.search(r'(href=\"magnet:.*nce\") title=\"Magnet link\"', details)
+#	if not match:
+#		return ''
+#	# Removing href and return
+#	return match.group(1).replace("href=\"","").replace("\"","")
 
 
 if __name__ == '__main__':
