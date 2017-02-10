77c82becfdf78bdddef5c9a6ba9925a752eede5a
Atreya Bain <atreyabain@gmail.com>
2017-02-10 11:54:41 +0530

    Fix requirements

--- cbot.py
+++ cbot.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python3
 from chatterbot import ChatBot
-#from cleverbot import Cleverbot
-#cb = Cleverbot()
 chatbot = ChatBot(
 	'Jack Carlson',
 	trainer='chatterbot.trainers.ChatterBotCorpusTrainer',
 	database="./database.json"
 )
-#chatbot.train("chatterbot.corpus.english")
 
 def ask(query):
 	reply = chatbot.get_response(query)
