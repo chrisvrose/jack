862e807c684a307b20836638b32c3aa39deec49f
Christopher Rose <christophervrose@gmail.com>
2018-02-07 01:29:37 +0530

    fix

--- messages.py
+++ messages.py
@@ -64,7 +64,7 @@ if(len(sys.argv)==2):
     global conn
     conn = psycopg2.connect(parseDBURI(sys.argv[1]))
     cur = conn.cursor()
-    if(!os.path.isfile('refresh_token.txt'):
+    if(!os.path.isfile('refresh_token.txt')):
         with open('refresh_token.txt) as file:
             print(cur.execute("SELECT * from reft;").fetchone())
     cur.close()
