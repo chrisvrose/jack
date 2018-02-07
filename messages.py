7f2c38b79a0d5e39d41c3a513fb02ba2a2558260
Christopher Rose <christophervrose@gmail.com>
2018-02-07 09:17:10 +0530

    String

--- messages.py
+++ messages.py
@@ -65,7 +65,7 @@ if(len(sys.argv)==2):
     conn = psycopg2.connect(parseDBURI(sys.argv[1]))
     cur = conn.cursor()
     if(not os.path.isfile('refresh_token.txt')):
-        with open('refresh_token.txt',w+) as file:
+        with open('refresh_token.txt','w+') as file:
             cur.execute('SELECT * from reft;')
             b = cur.fetchone()
             file.write(b[1])
