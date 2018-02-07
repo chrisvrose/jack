93447583ea2aefc87233e69b37036185985b4588
Christopher Rose <christophervrose@gmail.com>
2018-02-07 21:29:51 +0530

    Setup

--- cbot.py
+++ cbot.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python3
 from chatterbot import ChatBot
-chatbot = ChatBot(
-	'Jack Carlson',
-	trainer='chatterbot.trainers.ChatterBotCorpusTrainer',
-	database="./database.db"
-)
+import os
+
+global chatbot
+
+if 'DYNO' in os.environ:
+	chatbot = ChatBot(
+		'Jack Carlson',
+		trainer='chatterbot.trainers.ChatterBotCorpusTrainer',
+	        database_uri=os.environ('DATABASE_URL')
+	)
+else:
+	chatbot = ChatBot(
+                'Jack Carlson',
+                trainer='chatterbot.trainers.ChatterBotCorpusTrainer',
+                database="./database.json"
+        )
 
 def ask(query):
 	reply = chatbot.get_response(query)
