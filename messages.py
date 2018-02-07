ae4ea3f25082f4c143e2559df1a504fca86633a9
Christopher Rose <christophervrose@gmail.com>
2018-02-07 20:02:15 +0530

    Fix Stuff

--- messages.py
+++ messages.py
@@ -63,7 +63,12 @@ if(len(sys.argv)>1):
     cur = conn.cursor()
     if((not os.path.isfile('refresh_token.txt')) and len(sys.argv)==2):
         with open('refresh_token.txt','w+') as file:
-            cur.execute('SELECT * from reft;')
+            cur.execute('SELECT * from reft where typev=\'reft\';')
+            b = cur.fetchone()
+            file.write(b[1])
+    if((not os.path.isfile('database.json')) and len(sys.argv)==2):
+        with open('database.json','w+') as file:
+            cur.execute('SELECT * from reft where typev=\'dbf\';')
             b = cur.fetchone()
             file.write(b[1])
     cur.close()
@@ -261,6 +266,11 @@ def sigterm_handler(_signo=0, _stack_frame=0):
                     with open('refresh_token.txt','r') as file:
                         b = file.read()
                         cur.execute('update reft set storage=\''+b+'\' where typev=\'reft\';')
+                        conn.commit()
+                if(os.path.isfile('database.json')):
+                    with open('database.json','r') as file:
+                        b = file.read()
+                        cur.execute('update reft set storage=\''+b+'\' where typev=\'dbf\';')
                         conn.commit()
                 cur.close()
                 conn.close()
